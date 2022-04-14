e7865313e7b7aae72bdba1eb76fe28a97e6c4d99
Maximilian Ditz <max.ditz@outlook.de>
2022-04-14 22:36:08 +0200

    test commit

--- src/utils/ClientConfig.ts
+++ src/utils/ClientConfig.ts
@@ -1,4 +1,5 @@
 export const ClientConfig = {
     //serverURL: "https://fmpackagereception.azurewebsites.net/api", //Prduction URL
+    //Test comment
     serverURL: "http://localhost:5000/api",
 }
