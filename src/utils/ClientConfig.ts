8e6e5398491e6981bdcc3815f581b7427b82ae3e
Maximilian Ditz <max.ditz@outlook.de>
2022-04-22 11:47:20 +0200

    added firebase

--- src/utils/ClientConfig.ts
+++ src/utils/ClientConfig.ts
@@ -1,4 +1,4 @@
 export const ClientConfig = {
-    serverURL: "http://194.163.182.236:5000/api", //Prduction URL
+    serverURL: "https://194.163.182.236:5000/api", //Prduction URL
     // serverURL: "http://192.168.0.71:5000/api", //Dev URL
 }
